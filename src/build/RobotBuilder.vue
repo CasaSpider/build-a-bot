fbe7f256a4ef20340745ca22ca68482fd2858d9e
Nijhuis <nijhp1@bw.local>
2019-06-21 13:48:02 +0200

    forgot to save RobotBuilder.vue

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,19 +2,19 @@
   <div class="content">
     <div class="preview">
       <CollapsibleSection>
-      <div class="preview-content">
-        <div class="top-row">
-          <img :src="selectedRobot.head.src" />
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src" />
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left" />
+            <img :src="selectedRobot.torso.src" />
+            <img :src="selectedRobot.rightArm.src" class="rotate-right" />
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src" />
+          </div>
         </div>
-        <div class="middle-row">
-          <img :src="selectedRobot.leftArm.src" class="rotate-left" />
-          <img :src="selectedRobot.torso.src" />
-          <img :src="selectedRobot.rightArm.src" class="rotate-right" />
-        </div>
-        <div class="bottom-row">
-          <img :src="selectedRobot.base.src" />
-        </div>
-      </div>
       </CollapsibleSection>
       <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     </div>
